65700344035554b118b0a4c04f3cc3c02316ec68
Pieter-Jan Van Damme <pieterjan.vandamme.p4157@student.hogent.be>
2018-12-19 13:05:24 +0100

    feat: implemented and tested function getMentionsIndices

--- app/src/main/java/pjvandamme/be/jocelyn/Presentation/ComposeJottingActivity.kt
+++ app/src/main/java/pjvandamme/be/jocelyn/Presentation/ComposeJottingActivity.kt
@@ -30,17 +30,11 @@ class ComposeJottingActivity : AppCompatActivity() {
                 val selStart: Int = editTextField.selectionStart
                 val selEnd: Int = editTextField.selectionEnd
                 var wordAtCursor: String? = getWordAtCursor(editable,selStart,selEnd).toString()
-
-                /* testing */
-                val characterAfter: TextView = findViewById(R.id.characterAfter)
-                characterAfter.text = wordAtCursor
-
-                wordAtCursor = getWordAtCursor(editable,0,0).toString()
-                characterAfter.text = wordAtCursor
-
-                if (wordAtCursor!!.isNotEmpty() && wordAtCursor?.get(0)=='@')
-                    characterAfter.text = "Moniker detected!"
-                /* end testing */
+                if (wordAtCursor!!.isNotEmpty() && wordAtCursor?.get(0)=='@') {
+                    val mentions = getMentionsIndices(editable, '@')
+                    val after: TextView = findViewById(R.id.characterAfter)
+                    after.text = mentions.toString()
+                }
             }
 
             override fun beforeTextChanged(sequence: CharSequence?, start: Int, count: Int, after: Int) {
@@ -66,7 +60,7 @@ class ComposeJottingActivity : AppCompatActivity() {
      * A word can only be returned if the function is given a position (selStart == selEnd), not if it is given
      * a selection (selEnd > selStart).
      *
-     * @param editable The editable containing text.
+     * @param editable The editable containing the text.
      * @param selStart Starting index of the cursor/selection in the editable.
      * @param selEnd Ending index of the cursor/selection in the editable.
      * @return The word found at the given position, or null if the given indices form a selection.
@@ -119,5 +113,51 @@ class ComposeJottingActivity : AppCompatActivity() {
         }
         else
             return null
+    }
+
+    /**
+     * Finds the indices of (both inclusive) all words (separated by spaces) beginning with the given initChar
+     * in the given editable.
+     *
+     * For example: if the editable contains the text "Hello @Jocelyn and @Matt" and has the initChar '@', then
+     * the result will be [[6,13],[19,23]].
+     *
+     * It's important to note that a mention 1) is bordered by spaces (or the editable's boundaries), 2) begins with
+     * the initChar. An editable with the text "Hello @Jocelyn@Matt" and an initChar '@' given to this function will
+     * return [[6,18]] whereas the text "Hello @Jocelyn @Matt" with the same initChar would return [[6,13],[15,19]].
+     *
+     * @param editable The editable containing the text to be searched.
+     * @param initChar The character that marks the beginning of a mention.
+     * @return A 2-dimensional list containing the beginning and end positions of each mention.
+     */
+    fun getMentionsIndices(editable: Editable?, initChar: Char): List<List<Int>>{
+        val searchText = editable.toString()
+        var mentions: MutableList<List<Int>> = mutableListOf()
+        var count = 0
+        var position = 0
+        var endposition: Int
+
+        while(position < searchText.length) {
+            position = searchText.indexOf(initChar, position, true)
+            // break if no (further) mention was found
+            if (position == -1)
+                break
+            // continue if found character is not preceded by a space
+            if (position-1 >= 0 && searchText[position-1] != ' ') {
+                position++ // update position so next mention can be found, otherwise an endless loop occurs
+                continue
+            }
+
+            endposition = searchText.indexOf(' ', position, true)
+            if (endposition == -1) // mention is at the end of editable
+                endposition = searchText.length
+
+            mentions.add(count, listOf(position, endposition-1))
+
+            count++ // update count
+            position++ // update position so next mention can be found, otherwise an endless loop occurs
+        }
+
+        return mentions
     }
 }
